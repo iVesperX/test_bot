c8372715adc987d248ec2e0fde2c2f3ac4400c6a
iVesperX <vesperx9@outlook.com>
2018-05-13 16:20:08 -0400

    minor change

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const client = new Discord.Client();
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   for (let i = 0; i < files.length; i++) {
-    let event_file = require(`./events/${file[i]}`);
-    let event = file[i].split('.')[0];
+    let event_file = require(`./events/${files[i]}`);
+    let event = files[i].split('.')[0];
     client.on(event, (...args) => event_file.run(client, ...args));
   }
 });
